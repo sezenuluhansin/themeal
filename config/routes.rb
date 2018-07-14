c4333f72e3f63a5c8d1129772e8d1f326e8625f6
SEZEN ULUHANSIN <sezenuluhansin@hotmail.com>
2018-07-15 01:00:14 +0300

    restaurants

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get 'about', to:'pages#about'
+
+
+  get 'contact', to: 'pages#contact'
+
+  get 'restaurants', to: 'restaurants#index'
+
+
+  root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
