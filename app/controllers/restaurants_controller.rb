7089a5908fb2a5ad9e73ecc74bc5b76d621a95c3
SEZEN ULUHANSIN <sezenuluhansin@hotmail.com>
2018-07-27 20:45:05 +0300

    CRUD

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,23 +1,18 @@
 class RestaurantsController < ApplicationController
-  RESTAURANTS = {
-    1 => {name: "Dishroom", address: "London", category: "indian"},
-    2 => {name: "Sushi Samba", address: "Turkey", category: "japanese"}
-  }
-
-def show
-  raise
-end
-
- def index
-
-    if params[:food_type].blank?
-      @restaurants = RESTAURANTS
-    else
-      @restaurants = RESTAURANTS.select do |id, restaurant|
-       restaurant [:category] == params[:food_type]
-      end
-    end
+  def index 
+    @restaurants = Restaurants.all
   end
- def create
-   puts
+  def show
+  end
+
+  def new
+  end
+  def create
+  end
+  def edit
+  end
+  def update
+  end
+  def destoy
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,19 @@ Rails.application.routes.draw do
 
   get 'contact', to: 'pages#contact'
 
-  get 'restaurants/:id', to: 'restaurants#show'
-
   get 'restaurants', to: 'restaurants#index'
 
+  get 'restaurants/:id', to: 'restaurants#show'
+
+  get "restaurants/new", to: "restaurants#new"
+
   post 'restaurants', to: 'restaurants#create'
+
+  get 'restaurants/:id/edit', to: 'restaurants#edit'
+
+  patch 'restaurants/:id', to:'restaurants#update'
+
+  delete 'restaurants/:id', to: 'restaurants#destroy'
 
 
   root to: 'pages#home'
